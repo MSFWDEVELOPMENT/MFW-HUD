69e2ae2398adb25c253dc614228db59b40a59388
MOWGLI0001 <godsdevelopement@gmail.com>
2023-09-04 00:04:48 +0530

    ADDED V2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,15 +5,23 @@ author 'MOWGLIUwU#0215'
 description 'MFW-HUD BY MOWGLIUwU#0215'
 version '2.0.0'
 
-client_script 'config.lua'
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua'
+}
 client_script 'client.lua'
 server_script 'server.lua'
-lua54 'yes'
 
 ui_page 'html/index.html'
 
 files {
-	'html/index.html',
-	'html/styles.css',
-	'html/app.js'
+    'html/*',
+    'html/index.html',
+    'html/styles.css',
+    'html/responsive.css',
+    'html/app.js',
 }
+
+lua54 'yes'
